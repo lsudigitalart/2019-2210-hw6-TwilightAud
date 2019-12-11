d25337762cb3b7a0eeed0879db594fe08fa94a5a
Audrey McLellan <audrey15mclellan@gmail.com>
2019-12-11 10:49:17 -0600

    jhvfk

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let img;
+var img;
 x = 0;
 y = 0;
 w = 250;
 h = 400;
-let img2;
+var img2;
 x2 = 0;
 y2 = 0;
 w2 = 250;
@@ -13,17 +13,17 @@ x3 = 0;
 y3 = 0;
 w3 = 250;
 h3 = 400;
+
 function preload() {
   img = loadImage('IMG_0147.jpg');
-  //img2 = loadImage('IMG_0149.jpg');
+  // img2 = loadImage('IMG_0149.jpg');
  // img3 = loadImage('IMG_0152.jpg');
 }
 function setup() {
   createCanvas(displayWidth, displayHeight);
   // scale(.25);
-   image(img,0,0);
-  // image(img2,300,300);
-   //image(img3,400,400);
+  // image(img2,100,100);
+   //image(img3,200,200);
   // get the width and height of the image so we can center it initially
 //  bounds = img.imgBounds(image, 0, 0, imageSize);
 //  x = width / 2 - bounds.w / 2;
@@ -37,8 +37,8 @@ function draw() {
 
   //get its bounding box
 
-  //image(img, x, y, w, h);
-  //image(img2, x2, y2, w2, h2);
+  image(img, x, y, w, h);
+  // image(img2, x2, y2, w2, h2);
   //image(img3, x3, y3, w3, h3);
   // bounds = img.imgBounds(image, x, y, imageSize);
 
@@ -52,15 +52,15 @@ function draw() {
     x += random(-5, 5);
     y += random(-5, 5);
   }
-  //if (
-    //mouseX >= x2 &&
-    //mouseX <= x2 + w2 &&
-    //mouseY >= y2 &&
-    //mouseY <= y2 + h2
-  //) {
-    //x2 += random(-5, 5);
-    //y2 += random(-5, 5);
-  //}
+  // if (
+  //   mouseX >= x2 &&
+  //   mouseX <= x2 + w2 &&
+  //   mouseY >= y2 &&
+  //   mouseY <= y2 + h2
+  // ) {
+  //   x2 += random(-5, 5);
+  //   y2 += random(-5, 5);
+  // }
   //if (
     //mouseX >= x3 &&
     //mouseX <= x3 + w3 &&
